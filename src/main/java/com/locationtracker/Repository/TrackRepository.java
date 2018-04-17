e63dc3fc7f2760d8c0f7772d71bad48e7aef71e8
Mateusz Krzyżanowski <matkrzy94@gmail.com>
2018-04-17 16:37:03 +0200

    Add tracks endpoints

---
+++ src/main/java/com/locationtracker/Repository/TrackRepository.java
@@ -0,0 +1,16 @@
+package com.locationtracker.Repository;
+
+import com.locationtracker.Model.Track;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface TrackRepository extends CrudRepository<Track,Long> {
+    List<Track> findAll();
+
+    List<Track> findByRemovedIsFalse();
+
+    Track findById(int id);
+
+    Track save(Track track);
+}
