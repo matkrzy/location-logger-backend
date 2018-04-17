eec01d0f2271d34c40ab5a920cec97ee3b57f5ae
Mateusz Krzyżanowski <mateusz.krzyzanowski@thesoftwarehouse.pl>
2018-04-17 15:18:52 +0200

    Split controllers of devices

---
+++ src/main/java/com/locationtracker/Controller/DevicesController.java
@@ -0,0 +1,38 @@
+package com.locationtracker.Controller;
+
+import com.locationtracker.Model.Device;
+import com.locationtracker.Repository.DeviceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path ="/devices")
+public class DevicesController {
+    @Autowired
+    public DeviceRepository deviceRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody
+    List<Device> getAllActiveDevices() {
+
+        return deviceRepository.findByRemovedIsFalse();
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody
+    List<Device> getAllDevices() {
+
+        return deviceRepository.findAll();
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DevicesController.class, args);
+    }
+}
