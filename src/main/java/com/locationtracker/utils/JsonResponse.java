6d214659e21ba4a3a40b0ff1d4ec61e58c968a62
Mateusz Krzyżanowski <matkrzy94@gmail.com>
2018-04-29 23:46:11 +0200

    Add JsonResponse util

---
+++ src/main/java/com/locationtracker/utils/JsonResponse.java
@@ -0,0 +1,84 @@
+package com.locationtracker.utils;
+
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.http.HttpStatus;
+
+public class JsonResponse {
+    private JSONObject response = null;
+    private HttpStatus status = null;
+    private String message = null;
+
+    public JsonResponse() {
+        this.response = new JSONObject();
+    }
+
+    public void setStatus(HttpStatus status) {
+        try {
+            this.status = status;
+            this.response.put("status", status.toString());
+        } catch (Exception e) {
+            System.out.print(e.toString());
+        }
+    }
+
+    public void setMessage(String message) {
+        this.setStatus(HttpStatus.OK);
+
+        try {
+            this.response.put("message", message);
+        } catch (Exception e) {
+            System.out.print(e.toString());
+        }
+    }
+
+    public void setMessageError(String message) {
+        this.setStatus(HttpStatus.BAD_REQUEST);
+
+        try {
+            this.response.put("error", message);
+        } catch (Exception e) {
+            System.out.print(e.toString());
+        }
+    }
+
+    public void setMessageArray(String[] messages) {
+        this.setStatus(HttpStatus.OK);
+
+        try {
+            for (String message : messages) {
+                this.response.append("message", message);
+            }
+        } catch (Exception e) {
+            System.out.print(e.toString());
+        }
+    }
+
+    public void setMessageErrorArray(String[] messages) {
+        this.setStatus(HttpStatus.BAD_REQUEST);
+
+        try {
+            for (String message : messages) {
+                this.response.append("error", message);
+            }
+        } catch (Exception e) {
+            System.out.print(e.toString());
+        }
+    }
+
+    public JSONObject getMessageAsObject() {
+        return this.response;
+    }
+
+    public String getMessageAsString() {
+        return this.response.toString();
+    }
+
+    public HttpStatus getStatus(){
+        return this.status;
+    }
+
+    public String getMessage(){
+        return  null;
+    }
+
+}
